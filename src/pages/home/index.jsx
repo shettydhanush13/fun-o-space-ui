8ace0959c6b3063c0234673e2e7aef0529e13d6d
Dhanush Shetty <dhanushshetty@CIMUM001359.local>
2021-10-14 09:46:56 +0530

    feat: game lobby styling

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { Container } from 'reactstrap';
- 
+import './styles.scss'
+
 const DashBoard = () => {
 
   const history = useHistory()
@@ -15,16 +16,26 @@ const DashBoard = () => {
   
   return (
       <Container fluid className="p-0">
-        <button onClick={createRoom}>CREATE ROOM</button>
-        <br />
-        <br />
-        <span>{newRoomId}</span>
-        {newRoomId !== '' && <button onClick={() => history.push(`/room/${newRoomId}`)}>JOIN THE ROOM</button>}
-        <br />
-        <br />
-        <br />
-        <input type="text" value={roomId} onChange={e => setRoomId(e.target.value)} />
-        <button onClick={() => history.push(`/room/${roomId}`)}>JOIN ROOM</button>
+        <header className="gameHeader">
+          <div>
+              {/* <p>Room ID : {roomId}</p>
+              <p>Players Joined : {users.length}/4</p> */}
+          </div>
+          <div>
+              {/* <button onClick={() => users.length !== 4 ? alert('4 players required to start the game') : socket.emit('start-game', roomId, users)}>Start game</button>
+              <button onClick={() => socket.emit('leave-room', roomId)}>Leave room</button> */}
+          </div>
+        </header>
+        <div className="gameLobby">
+          <button onClick={createRoom}>CREATE ROOM</button>
+          {newRoomId !== '' && <span>ROOM ID : {newRoomId}</span>}
+          {newRoomId !== '' && <button className="joinGameButton" onClick={() => history.push(`/room/${newRoomId}`)}>JOIN GAME</button>}
+          <p>GOT A ROOM ID ?</p>
+          <div className="joinRoomSection">
+            <input type="text" placeholder='ENTER ROOM ID HERE' value={roomId} onChange={e => setRoomId(e.target.value)} />
+            <button onClick={() => history.push(`/room/${roomId}`)}>JOIN</button>
+          </div>
+        </div>
       </Container>
   );
 };
